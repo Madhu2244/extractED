afde54708e2ad343e11f7c60e891ddd9208a442f
Rhea Jethvani <rhearjethvani@gmail.com>
2024-04-20 03:17:43 -0700

    Basic File Upload Modal

--- frontend_new/npo-frontend-vite-template-main/src/App.jsx
+++ frontend_new/npo-frontend-vite-template-main/src/App.jsx
@@ -11,34 +11,13 @@ import {
   useDisclosure,
   Button
 } from '@chakra-ui/react'
+import FileUploadModal from './FileUploadModal';
 
 function App() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-
   return (
     <>
       <ChakraProvider>
-        <Button onClick={onOpen}>Open Modal</Button>
-
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              Hello World
-            </ModalBody>
-
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3} onClick={onClose}>
-                Close
-              </Button>
-              <Button variant='ghost'>Secondary Action</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-        {/* <ChakraUITest /> */}
-        {/* <UploadFile /> */}
+        <FileUploadModal />
       </ChakraProvider>
     </>
   );
